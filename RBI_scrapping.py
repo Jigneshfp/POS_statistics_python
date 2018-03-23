c2f844fa469e4592ed3a232f2d4866fa5ba582b0
Jignesh Patel <31859464+Jigneshfp@users.noreply.github.com>
2018-03-23 23:06:46 +0530

    Update RBI_scrapping.py

--- RBI_scrapping.py
+++ RBI_scrapping.py
@@ -5,4 +5,9 @@ import re
 url = "https://rbi.org.in/Scripts/ATMView.aspx"
 html_doc = requests.get(url)
 soup = BeautifulSoup(html_doc)
-url_list = soup.find_all('a', href = re.compile("http://rbidocs.rbi.org.in/rdocs/ATM/DOCs"))
+url_temp = soup.find_all('a', href = re.compile("http://rbidocs.rbi.org.in/rdocs/ATM/DOCs"))
+url_list = []
+for link in url_temp:
+  url_fetched = link.get('href')
+  url_list.append(url_fetched)
+
